b7e3b097c4b733814d019bf9c55629f8716aca7c
Sebastian Rodriguez Murillo <sebas907.l@gmail.com>
2024-04-25 13:08:09 -0500

    I finished the view model and made a design for a main activity and started the addPhotoActivity

--- app/src/main/java/com/srbastian/photoalbum/view/MainActivity.kt
+++ app/src/main/java/com/srbastian/photoalbum/view/MainActivity.kt
@@ -2,11 +2,28 @@ package com.srbastian.photoalbum.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.srbastian.photoalbum.R
+import com.srbastian.photoalbum.databinding.ActivityMainBinding
+import com.srbastian.photoalbum.viewmodel.MyImagesViewModel
 
 class MainActivity : AppCompatActivity() {
+    lateinit var myImagesViewModel: MyImagesViewModel
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding  = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainBinding.root)
+
+        myImagesViewModel = ViewModelProvider(this)[MyImagesViewModel::class.java]
+        myImagesViewModel.getAllImages().observe(this, Observer { images ->
+            // update UI
+        })
+
+        mainBinding.floatingActionButton.setOnClickListener {
+            // Open add imageActivity
+        }
     }
 }
--- app/src/main/java/com/srbastian/photoalbum/viewmodel/MyImagesViewModel.kt
+++ app/src/main/java/com/srbastian/photoalbum/viewmodel/MyImagesViewModel.kt
@@ -2,9 +2,37 @@ package com.srbastian.photoalbum.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import com.srbastian.photoalbum.model.MyImages
+import com.srbastian.photoalbum.repository.MyImagesRepository
+import kotlinx.coroutines.Dispatchers
 
 class MyImagesViewModel(application: Application) : AndroidViewModel(application) {
 
+    private var repository: MyImagesRepository
+    var imagesList: LiveData<List<MyImages>>
 
+    init {
+        repository = MyImagesRepository(application)
+        imagesList = repository.getAllImages()
+    }
+
+    fun insert(myImages: MyImages) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(myImages)
+    }
+
+    fun update(myImages: MyImages) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(myImages)
+    }
+
+    fun delete(myImages: MyImages) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(myImages)
+    }
+
+    fun getAllImages(): LiveData<List<MyImages>> {
+        return imagesList
+    }
 
 }
